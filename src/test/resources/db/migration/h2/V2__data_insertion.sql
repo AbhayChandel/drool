fdb57fc07bf196b7ebd5f0b7c2fecfaa2b2764d7
Abhay Chandel <abhay1072@gmail.com>
2019-12-31 00:32:50 +0530

    Fixed space issue in H2 sql files

--- src/test/resources/db/migration/h2/V2__data_insertion.sql
+++ src/test/resources/db/migration/h2/V2__data_insertion.sql
@@ -1,4 +1,4 @@
-// passwords were encoded with bCryptEncoders https://bcrypt-generator.com/
+-- passwords were encoded with bCryptEncoders https://bcrypt-generator.com/
 insert into user_account(email, password) values ('talk_to_priyanka@gmail.com', '$2y$12$bDHT1s2E8pH7if.XQr/14urBafxek/62kxe9R.omoloGhh9XnwdbW');
 insert into user_account(email, password) values ('priya.singh@gmail.com', '$2y$12$nfSQJzgpJ3gTu.CczB6BiuceNqA6niFi7EX03p4Ep3205kL5I2pDy');
 insert into user_account(email, password) values ('sonam99@gmail.com', '$2y$12$nkEeE1P.hWfg1iqhp8JWOea9F7lEEzBi07ZdGs1ujrVJM5YVYnQqi');
