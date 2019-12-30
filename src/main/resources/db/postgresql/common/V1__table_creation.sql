6eecf686d1c84e8f136f1f4a51bc078423e51638
Abhay Chandel <abhay1072@gmail.com>
2019-12-31 00:17:42 +0530

    Setup Test and Prod environment configurations

---
+++ src/main/resources/db/postgresql/common/V1__table_creation.sql
@@ -0,0 +1,74 @@
+CREATE TABLE user_account
+(
+    id       SERIAL,
+    email    varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    CONSTRAINT email_unique UNIQUE (email),
+    CONSTRAINT user_authentication_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE user_profile
+(
+    id       SERIAL,
+    username varchar(100) NOT NULL,
+    mobile   BIGINT,
+    city     varchar(100),
+    gender   CHAR,
+    CONSTRAINT username_unique UNIQUE (username),
+    CONSTRAINT user_profile_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE discussion_topic
+(
+    id      SERIAL,
+    topic   varchar(250)         NOT NULL,
+    user_id BIGINT               NOT NULL,
+    active  BOOLEAN default true NOT NULL,
+    CONSTRAINT discussion_topic_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE discussion_topic_activity
+(
+    discussion_topic_id BIGINT NOT NULL REFERENCES discussion_topic (id),
+    date_posted         TIMESTAMP,
+    date_last_active    TIMESTAMP,
+    views               INT default 0,
+    likes               INT default 0,
+    replies             INT default 0
+);
+
+CREATE TABLE discussion_topic_user_like
+(
+    user_id  BIGINT NOT NULL,
+    topic_id BIGINT NOT NULL
+);
+
+CREATE TABLE discussion_reply
+(
+    id                  SERIAL,
+    discussion_topic_id BIGINT,
+    reply               varchar(500)         NOT NULL,
+    user_id             BIGINT               NOT NULL,
+    active              BOOLEAN default true NOT NULL,
+    CONSTRAINT discussion_reply_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE discussion_reply_activity
+(
+    discussion_reply_id BIGINT NOT NULL REFERENCES discussion_reply (id),
+    date_posted         TIMESTAMP,
+    likes               INT default 0
+);
+
+CREATE TABLE discussion_reply_user_like
+(
+    user_id  BIGINT NOT NULL,
+    reply_id BIGINT NOT NULL
+);
+
+CREATE TABLE POST_TYPE
+(
+    post_type_id INT,
+    post_type    VARCHAR(50)
+);
+
