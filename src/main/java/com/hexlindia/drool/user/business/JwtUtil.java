e818f5a4c81e978287c15798cceccf45f0846ce5
Abhay Chandel <abhay1072@gmail.com>
2020-01-23 01:24:17 +0530

    Increased JWT token validity to seven days

--- src/main/java/com/hexlindia/drool/user/business/JwtUtil.java
+++ src/main/java/com/hexlindia/drool/user/business/JwtUtil.java
@@ -21,7 +21,7 @@ public class JwtUtil implements Serializable {
 
     private static final long serialVersionUID = -2550185165626007488L;
 
-    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60L;
+    private static final long JWT_TOKEN_VALIDITY = 7 * 24 * 60 * 60L;
 
     private final String secret;
 
