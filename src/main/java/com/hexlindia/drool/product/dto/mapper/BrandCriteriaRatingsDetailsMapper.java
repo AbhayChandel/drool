5296581d0f8f3ca502233233401c6795e9a78057
Abhay Chandel <abhay1072@gmail.com>
2020-03-30 16:51:32 +0530

    Added String to ObjectId mapping for id

--- src/main/java/com/hexlindia/drool/product/dto/mapper/BrandCriteriaRatingsDetailsMapper.java
+++ src/main/java/com/hexlindia/drool/product/dto/mapper/BrandCriteriaRatingsDetailsMapper.java
@@ -1,14 +1,17 @@
 package com.hexlindia.drool.product.dto.mapper;
 
+import com.hexlindia.drool.common.dto.mapper.ObjectIdMapper;
+import com.hexlindia.drool.common.dto.mapper.StringToObjectIdMapping;
 import com.hexlindia.drool.common.dto.mapper.UserRefMapper;
 import com.hexlindia.drool.product.data.doc.BrandCriteriaRatingsDetailsDoc;
 import com.hexlindia.drool.product.dto.BrandCriteriaRatingsDetailsDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {BrandCriterionRatingMapper.class, BrandRefMapper.class, UserRefMapper.class})
+@Mapper(componentModel = "spring", uses = {BrandCriterionRatingMapper.class, BrandRefMapper.class, UserRefMapper.class, ObjectIdMapper.class})
 public interface BrandCriteriaRatingsDetailsMapper {
 
+    @Mapping(source = "id", target = "id", qualifiedBy = StringToObjectIdMapping.class)
     @Mapping(source = "brandCriterionRatingDtoList", target = "brandCriterionRatingDocList")
     @Mapping(source = "brandRefDto", target = "brandRef")
     @Mapping(source = "userRefDto", target = "userRef")
