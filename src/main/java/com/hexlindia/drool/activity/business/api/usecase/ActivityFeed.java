0e280e0a79ad64e76e2234695f4164fec1964d99
Abhay Chandel <abhay1072@gmail.com>
2020-04-09 01:57:50 +0530

    Built service for activity feed 2

--- src/main/java/com/hexlindia/drool/activity/business/api/usecase/ActivityFeed.java
+++ src/main/java/com/hexlindia/drool/activity/business/api/usecase/ActivityFeed.java
@@ -1,8 +1,11 @@
 package com.hexlindia.drool.activity.business.api.usecase;
 
 import com.hexlindia.drool.activity.FeedDocFields;
+import com.hexlindia.drool.activity.dto.FeedDto;
 import com.hexlindia.drool.video.data.doc.VideoDoc;
 import org.bson.types.ObjectId;
+
+import java.util.List;
 
 public interface ActivityFeed {
 
@@ -11,4 +14,6 @@ public interface ActivityFeed {
     public void setField(ObjectId postId, FeedDocFields feedDocFields, String value);
 
     void incrementDecrementField(ObjectId postId, FeedDocFields feedDocFields, int value);
+
+    List<FeedDto> getFeed(int page);
 }
